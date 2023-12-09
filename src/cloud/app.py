27d66542e483019838eb1492abce9c329d2851d6
Sailesh Reddy Sirigireddy <sailesh2906@gmail.com>
2023-12-09 17:11:39 -0500

    new vals

--- src/cloud/app.py
+++ src/cloud/app.py
@@ -17,9 +17,9 @@ CLASSIFIER_URL = 'http://34.168.52.156:5000/classify'
 CHITCHAT_URL = 'http://34.168.123.224:5000/chat'
 RAG_URL = 'http://34.125.217.36:5000/rag'
 
-CHAT_VAL = "chat"
-FAREWELL_VAL = "bye"
-NOVELS_VAL = "novels"
+CHAT_VAL = "want to chat"
+FAREWELL_VAL = "conclude chat"
+NOVELS_VAL = "want to know about novels"
 
 BOOKS_MAP = {
     0: "The Adventures of Sherlock Holmes",
